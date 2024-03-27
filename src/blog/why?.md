92f938e251cf91dd1624648a8b5bccb0a5fb8b53
Akshat Shah <ashah1506@gmail.com>
2024-03-27 11:33:47 -0400

    some updates

--- src/blog/why?.md
+++
@@ -1,7 +0,0 @@
----
-path: "/first:)"
-date: "2021-09-03"
-title: "mY first blog post"
----
-
-It's either day one or one day.
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,25 +14,8 @@ const IndexPage = () => (
     <div className="introduction">
       <img className="pfpC" src={pfp} align="left" />
       <p>
-      Hey! I’m <code className="emph"><b>Akshat Shah</b></code>&nbsp;. I am a junior at 
-      the University of Pennsylvania studying Computer and Information Sciences. This summer, I was a software engineering intern at Techsmith
-      where I worked on Camtasia with the Voltron Pod. At school, I am currently a full-stack developer for <code className="emph"><b>PennApps</b></code>&nbsp;(the organizing community for our 
-      school hackathon) and  a teaching assistant for <code className="emph"><b> CIS 262 Automata., Computability, and Complexity</b></code>. 
-      </p>
-      {"\n"}
-      <p>
-      Throughout high school, I worked on research to help marginalized 
-      communities using <code className="emph"><b>computer vision</b></code>&nbsp;and <code className="emph"><b>machine learning</b></code>, but now I 
-      want to help bring my out-of-the-box thinking to integrate new 
-      technologies with companies! 
-      </p>
-      
-      <p>
-      I enjoy <code className="emph"><b>cooking</b></code>, finding underground <code className="emph"><b>music</b></code>, and partaking 
-      in activities that force me out of my <code className="emph"><b>comfort zone</b></code>&nbsp;(like my blog).
-      Since I've been in Philly, I've become an avid <code className="emph"><b>vegetarian</b></code>&nbsp;taco 
-      enthusiast and a <i>professional</i> spikeball player.
-      </p>
+      Hey! I’m Akshat.
+      </p>  
     </div>
   </Layout>
 )
